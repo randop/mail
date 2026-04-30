088137d18d5233bfce4a7e3fe5629e16453c952f
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2026-04-30 02:22:57 +0000

    perf(io): Use aligned allocation on DMA file writer

--- include/dma_file_writer.hpp
+++ include/dma_file_writer.hpp
@@ -11,19 +11,40 @@
 class dma_file_writer {
 public:
   explicit dma_file_writer(seastar::file f);
+  ~dma_file_writer();
+
+  dma_file_writer(const dma_file_writer &) = delete;
+  dma_file_writer &operator=(const dma_file_writer &) = delete;
+
+  dma_file_writer(dma_file_writer &&o) noexcept;
+  dma_file_writer &operator=(dma_file_writer &&o) noexcept;
 
   seastar::future<> write(const char *data, size_t size);
-
   seastar::future<> close();
 
 private:
+  struct segment {
+    const char *data;
+    size_t size;
+    size_t logical_size;
+  };
+
   seastar::file _file;
-  uint64_t _pos{0};
-  size_t _align{4096};
 
-  seastar::temporary_buffer<char> _buffer;
-  size_t _buf_used{0};
+  size_t _pos = 0;
+  size_t _align = 0;
 
-  seastar::future<> flush_aligned();
-  seastar::future<> write_all(const char *data, size_t size);
+  char *_buf = nullptr;
+  size_t _buf_used = 0;
+
+  static constexpr size_t batch_limit = 64;
+  std::vector<segment> _batch;
+
+private:
+  seastar::future<> flush_batch();
+  seastar::future<> drain_batch(bool final);
+
+  seastar::future<> write_physical(const char *data, size_t size);
+
+  void cleanup() noexcept;
 };
--- src/dma_file_writer.cpp
+++ src/dma_file_writer.cpp
@@ -1,8 +1,42 @@
 #include "dma_file_writer.hpp"
 
 dma_file_writer::dma_file_writer(seastar::file f) : _file(std::move(f)) {
-  _align = _file.disk_write_dma_alignment();
-  _buffer = seastar::temporary_buffer<char>(_align);
+
+  _align = std::max<size_t>(_file.disk_write_dma_alignment(), 4096);
+
+  _buf = static_cast<char *>(std::aligned_alloc(_align, _align));
+  if (!_buf) {
+    throw std::bad_alloc();
+  }
+}
+
+dma_file_writer::~dma_file_writer() { cleanup(); }
+
+dma_file_writer::dma_file_writer(dma_file_writer &&o) noexcept
+    : _file(std::move(o._file)), _pos(o._pos), _align(o._align), _buf(o._buf),
+      _buf_used(o._buf_used), _batch(std::move(o._batch)) {
+
+  o._buf = nullptr;
+  o._buf_used = 0;
+  o._pos = 0;
+}
+
+dma_file_writer &dma_file_writer::operator=(dma_file_writer &&o) noexcept {
+  if (this != &o) {
+    cleanup();
+
+    _file = std::move(o._file);
+    _pos = o._pos;
+    _align = o._align;
+    _buf = o._buf;
+    _buf_used = o._buf_used;
+    _batch = std::move(o._batch);
+
+    o._buf = nullptr;
+    o._buf_used = 0;
+    o._pos = 0;
+  }
+  return *this;
 }
 
 seastar::future<> dma_file_writer::write(const char *data, size_t size) {
@@ -12,35 +46,68 @@ seastar::future<> dma_file_writer::write(const char *data, size_t size) {
     size_t space = _align - _buf_used;
     size_t to_copy = std::min(space, size - offset);
 
-    memcpy(_buffer.get_write() + _buf_used, data + offset, to_copy);
+    std::memcpy(_buf + _buf_used, data + offset, to_copy);
 
     _buf_used += to_copy;
     offset += to_copy;
 
     if (_buf_used == _align) {
-      co_await flush_aligned();
+      co_await flush_batch();
     }
   }
 }
 
 seastar::future<> dma_file_writer::close() {
-  if (_buf_used == 0) {
+  if (_buf_used) {
+    std::memset(_buf + _buf_used, 0, _align - _buf_used);
+
+    _batch.push_back({_buf, _align, _buf_used});
+
+    _buf_used = 0;
+  }
+
+  co_await drain_batch(true);
+}
+
+seastar::future<> dma_file_writer::flush_batch() {
+  if (_buf_used) {
+    _batch.push_back({_buf, _buf_used, _buf_used});
+
+    _buf_used = 0;
+  }
+
+  co_await drain_batch(false);
+}
+
+seastar::future<> dma_file_writer::drain_batch(bool final) {
+  if (_batch.empty()) {
     co_return;
   }
 
-  // pad remaining bytes
-  memset(_buffer.get_write() + _buf_used, 0, _align - _buf_used);
+  size_t i = 0;
 
-  co_await write_all(_buffer.get(), _align);
-  _buf_used = 0;
+  while (i < _batch.size()) {
+    size_t end = std::min(i + batch_limit, _batch.size());
+
+    for (size_t j = i; j < end; ++j) {
+      const auto &s = _batch[j];
+      co_await write_physical(s.data, s.size);
+
+      _pos += s.logical_size;
+    }
+
+    i = end;
+  }
+
+  _batch.clear();
+
+  if (final) {
+    co_await _file.truncate(_pos);
+  }
 }
 
-seastar::future<> dma_file_writer::flush_aligned() {
-  co_await write_all(_buffer.get(), _align);
-  _buf_used = 0;
-}
-
-seastar::future<> dma_file_writer::write_all(const char *data, size_t size) {
+seastar::future<> dma_file_writer::write_physical(const char *data,
+                                                  size_t size) {
   size_t written = 0;
 
   while (written < size) {
@@ -52,5 +119,12 @@ seastar::future<> dma_file_writer::write_all(const char *data, size_t size) {
 
     written += n;
     _pos += n;
+  }
+}
+
+void dma_file_writer::cleanup() noexcept {
+  if (_buf) {
+    std::free(_buf);
+    _buf = nullptr;
   }
 }
